9ffed154afc5ebabe4e64d90c6481c1bc83a5c24
Maximilian Paju <maximilian.paju@gmail.com>
2025-11-24 13:00:22 +0100

    chore: clarify function description

--- csharp-code-test/ProblemA.cs
+++ csharp-code-test/ProblemA.cs
@@ -3,7 +3,7 @@ namespace csharp_code_test;
 public class ProblemA
 {
     /// <summary>
-    /// Given a year and month, and a collection of addresses, find the subscriptions on those addresses that are active.
+    /// Given a year and month, and a collection of addresses with associated subscriptions, find all the subscriptions that are active.
     /// A subscription is active when the subscription activation date (year and month) is either in the past relative to the given year and month, or when the subscription activation date is the exact given year and month.
     /// </summary>
     /// <param name="addresses">A enumerable of addresses</param>
