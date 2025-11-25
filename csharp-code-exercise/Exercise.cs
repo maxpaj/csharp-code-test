50c28566fa9ee018ee9fb8a72235f3a33c459945
Maximilian Paju <maximilian.paju@gmail.com>
2025-11-25 11:57:34 +0100

    chore: simplify exercise

---
+++ csharp-code-exercise/Exercise.cs
@@ -0,0 +1,29 @@
+namespace exercise;
+
+public class Exercise
+{
+    /// <summary>
+    /// Given a year and month, and a collection of addresses with associated 
+    /// subscriptions, find all the subscriptions that are active.
+    /// 
+    /// A subscription is active when the subscription activation 
+    /// date (year and month) is either in the past relative to the given year 
+    /// and month, or when the subscription activation date is the exact given 
+    /// year and month.
+    /// </summary>
+    /// <param name="addresses">A enumerable of addresses</param>
+    /// <param name="year">Any year</param>
+    /// <param name="month">Any month of a year (numbered 1 through 12)</param>
+    /// <returns>
+    /// Subscriptions on the given addresses that are active for the 
+    /// given year-month.
+    /// </returns>
+    public static IEnumerable<Subscription> GetActiveSubscriptionsDuringYearAndMonth(
+        IEnumerable<Address> addresses,
+        int year,
+        int month
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
